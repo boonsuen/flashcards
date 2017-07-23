87a188012eebbd621914d790633ad5ac5080d7f0
Boonsuen Oh <ohboonsuen123@gmail.com>
2017-07-23 23:32:44 +0800

    Updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const app = expess();
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
