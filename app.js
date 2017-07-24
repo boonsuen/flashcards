9abd04672dc096106c23f92b578a7d16e4b92d2b
Boonsuen Oh <ohboonsuen123@gmail.com>
2017-07-24 23:49:34 +0800

    Updated

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('The application is running on localhost:3000!')
 });
